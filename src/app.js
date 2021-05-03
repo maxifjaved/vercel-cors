72e70823d8fcdbb6a6796eb43efcecf258484e42
Muhammad Asif Javed <axif.javed@gmail.com>
2021-05-04 00:50:12 +0500

    cors-anywhare server created

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+
+
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
-const app = require('./app');
+const moduleAlias = require('module-alias');
+moduleAlias.addAlias('@', () => __dirname);
+
 const config = require('./config').value;
 const fs = require('fs');
-
+const corsProxy = require('cors-anywhere');
 const enableCluster = false;
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+
+const app = corsProxy.createServer({
+	originWhitelist: [], // Allow all origins
+	requireHeader: ['origin', 'x-requested-with'],
+	removeHeaders: ['cookie', 'cookie2']
+});
 
 if (enableCluster && cluster.isMaster && process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'dev') {
 	for (let i = 0; i < numCPUs; i++) {
 		cluster.fork();
 	}
 } else {
-// 	let server;
-// 	if (config.connect.socket) {
-// 		if (fs.existsSync(config.connect.socket)) {
-// 			fs.unlinkSync(config.connect.socket);
-// 		}
-// 		server = app.listen(config.connect.socket, parseInt(config.listenInaddrAny) ? null : '127.0.0.1');
-// 		logger.info('Listening Unix Socket ' + config.connect.socket);
-// 		process.on('SIGINT', () => {
-// 			fs.unlinkSync(config.connect.socket);
-// 			process.exit();
-// 		});
-// 	}
 	if (config.connect.port) {
 		app.listen(config.connect.port, parseInt(config.listenInaddrAny) ? null : '127.0.0.1');
 		console.info('Listening Port ' + config.connect.port);
 	}
-
-// logger.info('🎉 Vercel Server start! Cheers!');
 
 	module.exports = app;
 }
